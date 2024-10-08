32fbe17d322c73dda3efd56fe671d263f0b8a043
TeapotTerrance <josephjones1702@gmail.com>
2024-10-08 15:55:38 +0100

    Version 2

--- Scripts.js
+++ Scripts.js
@@ -15,43 +15,54 @@ fetch(apiURL)
         return response.json();
     })
     .then(data =>{
-        const temperature = data.main.temp;
-        const humidity = data.main.humidity;
-        const feelsLike = data.main.feels_like;
-        const visibilityKm = data.visibility/1000;
+
+        //Defining the variables for the display
+
+        function generalDisplayData(){
+            const temperature = data.main.temp;
+            const humidity = data.main.humidity;
+            const feelsLike = data.main.feels_like;
+            const visibilityKm = data.visibility/1000;
 
 
-        temperatureHTML.innerHTML += '\n' + temperature + '&#xb0;c';
-        humidityHTML.innerHTML += '\n' + humidity + '%';
-        feelsLikeHTML.innerHTML += '\n' + feelsLike + '&#xb0;c';
-        visibiltyHTML.innerHTML += '\n' + visibilityKm +'km';
-
+            temperatureHTML.innerHTML += '\n' + temperature + '&#xb0;c';
+            humidityHTML.innerHTML += '\n' + humidity + '%';
+            feelsLikeHTML.innerHTML += '\n' + feelsLike + '&#xb0;c';
+            visibiltyHTML.innerHTML += '\n' + visibilityKm +'km';
+        };
+        generalDisplayData();
         
 
-        let sunriseUNIX = data.sys.sunrise;
+        function sunriseTime(){
+            let sunriseUNIX = data.sys.sunrise;
         
-        const sunriseHTML = document.getElementById("sunriseTime");
-      
-        var sunrise = new Date(sunriseUNIX * 1000);
-        var hours = sunrise.getHours().toString().padStart(2, "0");
-        var minutes = sunrise.getMinutes().toString().padStart(2, "0");
-        var seconds = sunrise.getSeconds().toString().padStart(2, "0");
+            const sunriseHTML = document.getElementById("sunriseTime");
+        
+            var sunrise = new Date(sunriseUNIX * 1000);
+            var hours = sunrise.getHours().toString().padStart(2, "0");
+            var minutes = sunrise.getMinutes().toString().padStart(2, "0");
+            var seconds = sunrise.getSeconds().toString().padStart(2, "0");
 
-        var sunriseFormatted = hours + ":" + minutes + ":" + seconds;
-        sunriseHTML.innerHTML = sunriseFormatted;
+            var sunriseFormatted = hours + ":" + minutes + ":" + seconds;
+            sunriseHTML.innerHTML = sunriseFormatted;
+        };
+        sunriseTime();
+        
+        function sunsetTime(){
+            let sunsetUNIX = data.sys.sunset;
 
+            const sunsetHTML = document.getElementById("sunsetTime");
+        
+            var sunset = new Date(sunsetUNIX * 1000);
+            var hours = sunset.getHours().toString().padStart(2, "0");
+            var minutes = sunset.getMinutes().toString().padStart(2, "0");
+            var seconds = sunset.getSeconds().toString().padStart(2, "0");
 
-        let sunsetUNIX = data.sys.sunset;
-
-        const sunsetHTML = document.getElementById("sunsetTime");
-      
-        var sunset = new Date(sunsetUNIX * 1000);
-        var hours = sunset.getHours().toString().padStart(2, "0");
-        var minutes = sunset.getMinutes().toString().padStart(2, "0");
-        var seconds = sunset.getSeconds().toString().padStart(2, "0");
-
-        var sunsetFormatted = hours + ":" + minutes + ":" + seconds;
-        sunsetHTML.innerHTML = sunsetFormatted;
+            var sunsetFormatted = hours + ":" + minutes + ":" + seconds;
+            sunsetHTML.innerHTML = sunsetFormatted;
+        };
+        sunsetTime();
+        
         
     })
 
@@ -71,8 +82,51 @@ let currentTimeInSeconds = currentHoursInSeconds + currentMinutesInSeconds + cur
 console.log(currentTimeInSeconds);
 let startAnimation = -(86400-currentTimeInSeconds);
 
-let dayPercentage = (currentTimeInSeconds/86400)*100
-console.log(startAnimation)
+let dayPercentage = (currentTimeInSeconds/86400)*100;
+console.log(dayPercentage);
 
 root = document.documentElement;
 root.style.setProperty("--negative-delay", startAnimation + "s");
+
+
+//Animating the display
+document.addEventListener("DOMContentLoaded",domLoaded,false);
+function domLoaded(){
+
+    
+
+    const canvas = document.getElementById("canvasSunMoon");
+    const ctx = canvas.getContext("2d");
+    ctx.translate(canvas.width/2,canvas.height/2);
+
+
+    function ellipsePath(){
+        ctx.save();
+        ctx.beginPath();
+        ctx.ellipse(0, 0, 340, 50, Math.PI / 1, 0, 2 * Math.PI);
+        ctx.setLineDash([5,5]);
+        ctx.strokeStyle = "white";
+    
+        ctx.stroke();
+        ctx.restore();
+    };
+    ellipsePath();
+
+    function drawSun(){
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(0,0);
+        
+        ctx.fillStyle = "orange";
+        ctx.arc(0, 0, 40, 0, 2*Math.PI, false);
+        ctx.fill();
+        ctx.restore();
+        
+    };
+    drawSun();
+
+    function drawMoon(){
+
+    }
+}
+
